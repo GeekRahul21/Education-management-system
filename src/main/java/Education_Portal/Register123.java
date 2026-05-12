2ebadea3ff9799f4039ec1141dafbcd625a252cb
GeekRahul21 <rayalrahul314@gmail.com>
2026-05-12 13:26:08 +0530

    Fixed Register Servlet

--- src/main/java/Education_Portal/Register123.java
+++ src/main/java/Education_Portal/Register123.java
@@ -13,33 +13,33 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/RegisterServlet")
 public class Register123 extends HttpServlet {
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-		String fn=request.getParameter("fullname");
-		String em=request.getParameter("email");
-		String ur=request.getParameter("username");
-		String ps=request.getParameter("password");
-		
-		try {
-			
-			Connection con=DB_Connection.getConn();
-			
-			PreparedStatement  ps1=con.prepareStatement("insert into Staff(fullname,email,username,password) values(?,?,?,?)");
-			
-			ps1.setString(1, fn);
-			ps1.setString(2, em);
-			ps1.setString(3, ur);
-			ps1.setString(4, ps);
-			
-			ps1.executeUpdate();
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-		
-		response.sendRedirect("Login123.html");
-		
-	}
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String fn = request.getParameter("fullname");
+        String em = request.getParameter("email");
+        String ur = request.getParameter("username");
+        String ps = request.getParameter("password");
+
+        try {
+
+            Connection con = DB_Connection.getConn();
+
+            PreparedStatement ps1 = con.prepareStatement(
+                    "insert into Staff(fullname,email,username,password) values(?,?,?,?)");
+
+            ps1.setString(1, fn);
+            ps1.setString(2, em);
+            ps1.setString(3, ur);
+            ps1.setString(4, ps);
+
+            ps1.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        response.sendRedirect("Login123.html");
+    }
 }
