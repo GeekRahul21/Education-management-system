3adeb5a4276bf5d01b4bf95f22b089ef2063f757
GeekRahul21 <rayalrahul314@gmail.com>
2026-05-12 12:30:56 +0530

    second commit

--- src/main/java/Database_Connection/DB_Connection.java
+++ src/main/java/Database_Connection/DB_Connection.java
@@ -4,23 +4,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DB_Connection {
-	
-	public static Connection getConn() {
-		
-		Connection con=null;
-		try {
-			
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/EducationDB","root","rayal_goluExe@2324");
-			
-			
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-		
-		return con;
-	}
 
+    public static Connection getConn() {
+
+        Connection con = null;
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            String url = System.getenv("DB_URL");
+            String user = System.getenv("DB_USER");
+            String pass = System.getenv("DB_PASSWORD");
+
+            con = DriverManager.getConnection(url, user, pass);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return con;
+    }
 }
